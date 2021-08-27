d922ffb6a8e50c0b944d24e9754cc91d8c08c5bd
znk-bit <Nycolas.kevin2@gmail.com>
2021-08-27 11:27:07 -0300

    Teste de mesa

--- Variaveis.py
+++ Variaveis.py
@@ -2,9 +2,18 @@
 """ Para definir uma variavel utilizamos o "=" e se ler recebe
 """
 
-nota1 = 7.4
+"""nota1 = 7.4
 nota2 = 8.0
 
 media = (nota1 + nota2) / 2
 
-print(media)
+print(media)"""
+
+A = 1
+print(f"Esse é o valor atual de A: {A}")
+B = A + 3
+print(f"Esse é o valor de B: {B}")
+C = B + 3
+print(f"Este é o valor de C: {C}")
+A = C + B
+print(f"Este é o valor de A depois da alteração: {A}")
