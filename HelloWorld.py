7159c7dd705971f3af79684154151698a37f2a17
znk-bit <Nycolas.kevin2@gmail.com>
2021-08-27 10:36:44 -0300

    Aula dia 27/08

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,4 @@
+# Aula 1 | Teste do ambiente e hello world
+
+print(18)
 print('Hello world')
---
+++ Variaveis.py
@@ -0,0 +1,10 @@
+# Aula 2 | Variáveis 
+""" Para definir uma variavel utilizamos o "=" e se ler recebe
+"""
+
+nota1 = 7.4
+nota2 = 8.0
+
+media = (nota1 + nota2) / 2
+
+print(media)
