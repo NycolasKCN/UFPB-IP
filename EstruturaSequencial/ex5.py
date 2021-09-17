3f0866ddb909cb20aea2e4c6896d4d093f97aba8
znk-bit <Nycolas.kevin2@gmail.com>
2021-09-17 14:44:12 -0300

    ex5

---
+++ EstruturaSequencial/ex5.py
@@ -0,0 +1,7 @@
+"""Faça um Programa que converta metros para centímetros."""
+
+metro = float(input("Digite a metragem a ser convertida: M"))
+
+centimetro = metro * 100
+
+print(f"Após converção o valor em metros {metro:.1f} passou a ser {centimetro:.1f}cm")
