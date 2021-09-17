3dbc5854c71171775fab9d8c29fd7d75a653a295
znk-bit <Nycolas.kevin2@gmail.com>
2021-09-17 14:56:39 -0300

    ex6

---
+++ EstruturaSequencial/ex6.py
@@ -0,0 +1,7 @@
+"""Faça um Programa que peça o raio de um círculo, calcule e mostre sua área."""
+
+raio = float(input("Digite o raio do circulo (em centimetros): "))
+
+area = 3.14 * (raio ** 2)
+
+print(f"A área do circulo será: {area:.2f} cm")
