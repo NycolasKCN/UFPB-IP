6991d42baacd7cb923c4328643c8d6441901eecc
znk-bit <Nycolas.kevin2@gmail.com>
2021-09-17 14:27:00 -0300

    ex3

---
+++ EstruturaSequencial/ex3.py
@@ -0,0 +1,7 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro: "))
+soma = num1 + num2
+
+print(f"A soma de {num1} e {num2} é {soma}")
