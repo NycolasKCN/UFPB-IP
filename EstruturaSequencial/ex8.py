036e17e0e27db38b15fa7a93d493424300536d91
znk-bit <Nycolas.kevin2@gmail.com>
2021-09-17 15:12:25 -0300

    rx8

---
+++ EstruturaSequencial/ex8.py
@@ -0,0 +1,9 @@
+"""Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês."""
+
+valorPorHora = float(input("Digite quanto você ganha por hora: R$"))
+horaTrabalhada = int(input("Digite a quantida de horas trabalhadas nesse mês: "))
+
+salario = horaTrabalhada * valorPorHora
+
+print(f"No final do mês o seu salário será de: R${salario:.2f}")
